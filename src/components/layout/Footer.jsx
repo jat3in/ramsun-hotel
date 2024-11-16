11a0e65c3fceb1b11bdccca21d1a5ec953275677
divyanshu_sahu <divyanshusahucoc@gmail.com>
2024-11-16 13:38:57 +0530

    Footer added

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+
+<footer>
+
+<div class="footer bg-[#f2f2f2] flex flex-col sm:flex-row w-full justify-evenly flex-wrap pl-5 sm:pl-0">
+
+<div class="1">
+  <p class="font-semibold mt-8">What's new</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft 365</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Games</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Surface Pro 9</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Surface Laptop 5</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Surface Laptop Studio</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Surface Laptop Go 2</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Windows 11 apps</p>
+</div>
+
+<div class="2">
+  <p class="font-semibold mt-8">Microsoft Store</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Account Profile</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Download Center</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Store Support</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Returns</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Order Tracking</p>
+</div>
+<div class="3">
+  <p class="font-semibold mt-8">Education</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft in education</p>
+  <p class="my-3 text-xs text-gray-600  hover:underline cursor-pointer" >Devices for education</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft terms for<br/> education</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft 365 education</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Office education</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Educator traning and <br/>development</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Deals for students and <br/>parents</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Azure for students</p>
+</div>
+<div class="4">
+  <p class="font-semibold mt-8">Business</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Cloud</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Security</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Azure</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Dynamics 365 </p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft 365</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Advertising</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Industry</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Teams</p>
+
+</div>
+<div class="5">
+  <p class="font-semibold mt-8">Developer & IT</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Developer Center </p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Documentation </p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft Learn</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft tech community</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Azure Marketplace</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">App Source</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Microsoft power platform</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Visual Studio </p>
+  
+</div>
+<div class="6">
+  <p class="font-semibold mt-8">Company</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Carrers</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">About Microsoft </p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Company news</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Privacy at Microsoft</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Investors</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Security</p>
+  <p class="my-3 text-xs text-gray-600 hover:underline cursor-pointer">Sustainablity</p>
+</div>
+
+
+
+
+</div>
+
+<div class="pl-5 py-5 sm:px-10 bg-[#f2f2f2] sm:flex sm:justify-between">
+      <ul class="sm:flex">
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer"><i class="fa fa-globe pr-1" > </i> English (India)</li>
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer sm:pl-3">
+            <img src="" alt="logo" class="inline-block h-5"/> Your Privacy Choices</li>
+      </ul>
+      <ul class="flex flex-wrap ">
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer  ">Contact Microsoft</li>
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer  ">Privacy</li>
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer">Terms of use</li>      
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer">Trademarks</li>
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer">About our ads</li>
+        <li class="my-3 mx-1 text-xs text-gray-600 hover:underline cursor-pointer ">@Microsoft 2023</li>        
+      </ul>
+    </div>
+</footer>
+
+
+  )
+}
+
+export default Footer
